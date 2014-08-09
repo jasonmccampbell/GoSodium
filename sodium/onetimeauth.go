6fe6bfa72aea9fd843b49765ab8275b4d243c425
Jason McCanmpbell <jasonmccampbell@gmail.com>
2014-08-09 19:15:20 +0000

    Cleanup from code review, 'go fmt'.

--- sodium/onetimeauth.go
+++ sodium/onetimeauth.go
@@ -1,6 +1,5 @@
 package sodium
 
-//import "fmt"
 import "unsafe"
 
 // #cgo CFLAGS: -I/home/action/.parts/packages/libsodium/0.6.0/include
@@ -14,21 +13,20 @@ import "unsafe"
 import "C"
 
 // Opaque structure representing the internal state of the MAC calculations.
-type OneTimeAuthState C.struct_onetimeauth_poly1305_state
-
+type OneTimeAuthState unsafe.Pointer
 
 // OneTimeAuthBytes returns the size of the authenticator in bytes.
-func OneTimeAuthBytes () int {
-    return int(C.crypto_onetimeauth_bytes())
+func OneTimeAuthBytes() int {
+	return int(C.crypto_onetimeauth_bytes())
 }
 
 // OneTimeAuthKeyBytes specifies the number of bytes in the symmetric key
-func OneTimeAuthKeyBytes () int {
-    return int(C.crypto_onetimeauth_keybytes())
+func OneTimeAuthKeyBytes() int {
+	return int(C.crypto_onetimeauth_keybytes())
 }
 
 // OneTimeAuthPrimivite returns a string identifying which authentication primitive is being used.
-// TODO: Not found in my build. 
+// TODO: Not found in my build?! Odd.
 //func OneTimeAuthPrimitive () string {
 //    return C.GoString(crypto_onetimeauth_primitive())
 //}
@@ -38,59 +36,55 @@ func OneTimeAuthKeyBytes () int {
 // any other messages.
 //
 // Returns: 0
-func OneTimeAuth (macOut []byte, message []byte, key []byte) int {
-    checkSize(macOut, OneTimeAuthBytes(), "MAC output buffer")
-    checkSize(key, OneTimeAuthKeyBytes(), "key")
+// TODO: Can this ever return non-zero? If not should not return a value.
+func OneTimeAuth(macOut []byte, message []byte, key []byte) int {
+	checkSize(macOut, OneTimeAuthBytes(), "MAC output buffer")
+	checkSize(key, OneTimeAuthKeyBytes(), "key")
 
-    return int(C.crypto_onetimeauth(
-        (*C.uchar)(&macOut[0]),
-        (*C.uchar)(&message[0]), (C.ulonglong)(len(message)),
-        (*C.uchar)(&key[0])))
+	return int(C.crypto_onetimeauth(
+		(*C.uchar)(&macOut[0]),
+		(*C.uchar)(&message[0]), (C.ulonglong)(len(message)),
+		(*C.uchar)(&key[0])))
 }
-
-
 
 // OneTimeAuthVerify verifies a message authentication code (MAC) for a given message and key.
 //
 // Returns: 0 if the MAC authenticates the message, -1 if not.
 func OneTimeAuthVerify(mac, message, key []byte) int {
-    checkSize(mac, OneTimeAuthBytes(), "MAC")
-    checkSize(key, OneTimeAuthKeyBytes(), "key")
-    
-    return int(C.crypto_onetimeauth_verify(
-        (*C.uchar)(&mac[0]),
-        (*C.uchar)(&message[0]), (C.ulonglong)(len(message)),
-        (*C.uchar)(&key[0])))
-}
+	checkSize(mac, OneTimeAuthBytes(), "MAC")
+	checkSize(key, OneTimeAuthKeyBytes(), "key")
 
+	return int(C.crypto_onetimeauth_verify(
+		(*C.uchar)(&mac[0]),
+		(*C.uchar)(&message[0]), (C.ulonglong)(len(message)),
+		(*C.uchar)(&key[0])))
+}
 
 // OneTimeAuthInit initializes an internal state structure to allow incremental
 // computation of a message authentication code.
-func OneTimeAuthInit(key []byte) *OneTimeAuthState {
-    state := C._allocOneTimeAuthState()
-    C.crypto_onetimeauth_init( state, (*C.uchar)(&key[0]))
-    return (*OneTimeAuthState)((unsafe.Pointer)(state))
+// Warning: variance from libsodium: state is allocated here rather than passed in.
+func OneTimeAuthInit(key []byte) OneTimeAuthState {
+	state := C._allocOneTimeAuthState()
+	C.crypto_onetimeauth_init(state, (*C.uchar)(&key[0]))
+	return (OneTimeAuthState)(state)
 }
-
 
 // OneTimeAuthUpdate incrementally updates the MAC computation state with the contents of
 // inBuf. Update may be called multiple times while consuming a message.
-func OneTimeAuthUpdate(state *OneTimeAuthState, inBuf []byte) {
-    statePtr := (unsafe.Pointer)(state)
-    C.crypto_onetimeauth_update( (*C.struct_crypto_onetimeauth_poly1305_state)(statePtr), 
-                                (*C.uchar)(&inBuf[0]), (C.ulonglong)(len(inBuf)))
+func OneTimeAuthUpdate(state OneTimeAuthState, inBuf []byte) {
+	C.crypto_onetimeauth_update((*C.struct_crypto_onetimeauth_poly1305_state)(state),
+		(*C.uchar)(&inBuf[0]), (C.ulonglong)(len(inBuf)))
 }
 
 // OneTimeAuthFinal writes the final message authentication code to macOut based on the
 // state computed by OneTimeAuthInit and OneTimeAuthUpdate. Once this function is called
 // state is deallocated and may never be accessed again; this function must be called
 // exactly once for each call to OneTimeAuthInit to avoid leaking memory.
-func OneTimeAuthFinal(state *OneTimeAuthState, macOut []byte) int {
-    checkSize(macOut, OneTimeAuthBytes(), "MAC output buffer")
-    
-    statePtr := (unsafe.Pointer)(state)
-    r := int(C.crypto_onetimeauth_final( (*C.struct_crypto_onetimeauth_poly1305_state)(statePtr), 
-                                        (*C.uchar)(&macOut[0])))
-    C.free(statePtr)
-    return r
+func OneTimeAuthFinal(state OneTimeAuthState, macOut []byte) int {
+	checkSize(macOut, OneTimeAuthBytes(), "MAC output buffer")
+
+	r := int(C.crypto_onetimeauth_final((*C.struct_crypto_onetimeauth_poly1305_state)(state),
+		(*C.uchar)(&macOut[0])))
+	C.free((unsafe.Pointer)(state))
+	return r
 }
--- sodium/onetimeauth_test.go
+++ sodium/onetimeauth_test.go
@@ -3,47 +3,46 @@ package sodium
 import "testing"
 
 func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
-
 func TestOneTimeAuth(t *testing.T) {
-    // Scattering of message sizes and likely corner cases.
-    messageSizes := []int { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 63, 64,
-        65, 95, 96, 97, 127, 128, 129, 1023, 1024, 1025 }
-    const maxMessageSize = 1025
+	// Scattering of message sizes and likely corner cases.
+	messageSizes := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+		17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 63, 64,
+		65, 95, 96, 97, 127, 128, 129, 1023, 1024, 1025}
+	const maxMessageSize = 1025
 
-    msgBuf := make([]byte, maxMessageSize)
-    mac1 := make([]byte, OneTimeAuthBytes())
-    mac2 := make([]byte, OneTimeAuthBytes())
-    key := make([]byte, OneTimeAuthKeyBytes())
-    
-    RandomBytes(key)
-    RandomBytes(msgBuf)
-    for _, size := range messageSizes {
-        t.Log("Running message size ", size)
-        msg := msgBuf[:size]
-        
-        // Simple way, compute the MAC all at once.
-        OneTimeAuth(mac1, msg, key)
-        
-        // Compute the MAC incrementally.
-        state := OneTimeAuthInit(key)
-        for  i := 0; i < len(msg); i += 7 {
-            OneTimeAuthUpdate(state, msg[i:min(i+7, len(msg))])
-        }
-        OneTimeAuthFinal(state, mac2)
-        
-        // The MAC should be the same independent of how it was created.
-        if OneTimeAuthVerify(mac1, msg, key) != 0 {
-            t.Fatal("Failed to verify mac1 computed as a single-shot")
-        }
-        if OneTimeAuthVerify(mac2, msg, key) != 0 {
-            t.Fatal("Failed to verify mac2 computed incrementally")
-        }
-    }
+	msgBuf := make([]byte, maxMessageSize)
+	mac1 := make([]byte, OneTimeAuthBytes())
+	mac2 := make([]byte, OneTimeAuthBytes())
+	key := make([]byte, OneTimeAuthKeyBytes())
+
+	RandomBytes(key)
+	RandomBytes(msgBuf)
+	for _, size := range messageSizes {
+		t.Log("Running message size ", size)
+		msg := msgBuf[:size]
+
+		// Simple way, compute the MAC all at once.
+		OneTimeAuth(mac1, msg, key)
+
+		// Compute the MAC incrementally.
+		state := OneTimeAuthInit(key)
+		for i := 0; i < len(msg); i += 7 {
+			OneTimeAuthUpdate(state, msg[i:min(i+7, len(msg))])
+		}
+		OneTimeAuthFinal(state, mac2)
+
+		// The MAC should be the same independent of how it was created.
+		if OneTimeAuthVerify(mac1, msg, key) != 0 {
+			t.Fatal("Failed to verify mac1 computed as a single-shot")
+		}
+		if OneTimeAuthVerify(mac2, msg, key) != 0 {
+			t.Fatal("Failed to verify mac2 computed incrementally")
+		}
+	}
 }
