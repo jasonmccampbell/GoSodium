6ff98bbc2217d17453ebced0142d870b01c6566b
Graham Smith <neuegram@gmail.com>
2014-08-22 15:52:33 -0500

    Added support for crypto_generichash_blake2b_salt_personal

--- sodium/generichash.go
+++ sodium/generichash.go
@@ -32,8 +32,16 @@ func GenericHashKeyBytes() int {
 	return int(C.crypto_generichash_keybytes_max())
 }
 
+func GenericHashSaltBytes() int {
+	return int(C.crypto_generichash_blake2b_saltbytes())
+}
+
+func GenericHashPersonalBytes() int {
+	return int(C.crypto_generichash_blake2b_personalbytes())
+}
+
 func GenericHash(hashOut []byte, message []byte, key []byte) int {
-	checkSize(hashOut, GenericHashBytes(), "hash output")
+	checkHashOutSize(hashOut)
 	checkKeySize(key)
 	if key == []byte(nil) {
 		return int(C.crypto_generichash(
@@ -45,6 +53,34 @@ func GenericHash(hashOut []byte, message []byte, key []byte) int {
 		(*C.uchar)(&hashOut[0]), (C.size_t)(len(hashOut)),
 		(*C.uchar)(&message[0]), (C.ulonglong)(len(message)),
 		(*C.uchar)(&key[0]), (C.size_t)(len(key))))
+}
+
+func GenericHashSaltPersonal(hashOut []byte, message []byte, key []byte, salt []byte, personal []byte) int {
+	checkHashOutSize(hashOut)
+	checkKeySize(key)
+	checkSize(salt, GenericHashSaltBytes(), "salt")
+	checkSize(personal, GenericHashPersonalBytes(), "personal")
+	if key == []byte(nil) {
+		return int(C.crypto_generichash_blake2b_salt_personal(
+			(*C.uchar)(&hashOut[0]), (C.size_t)(len(hashOut)),
+			(*C.uchar)(&message[0]), (C.ulonglong)(len(message)),
+			nil, (C.size_t)(0),
+			(*C.uchar)(&salt[0]),
+			(*C.uchar)(&personal[0])))
+	}
+	return int(C.crypto_generichash_blake2b_salt_personal(
+		(*C.uchar)(&hashOut[0]), (C.size_t)(len(hashOut)),
+		(*C.uchar)(&message[0]), (C.ulonglong)(len(message)),
+		(*C.uchar)(&key[0]), (C.size_t)(len(key)),
+		(*C.uchar)(&salt[0]),
+		(*C.uchar)(&personal[0])))
+}
+
+func checkHashOutSize(hashOut []byte) {
+	if !(len(hashOut) > GenericHashBytesMin() && len(hashOut) < GenericHashBytesMax()) {
+		panic(fmt.Sprintf("Incorrect hash out buffer size, expected (%d - %d), got (%d).",
+			GenericHashBytesMin(), GenericHashBytesMax(), len(hashOut)))
+	}
 }
 
 func checkKeySize(key []byte) {
